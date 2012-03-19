2e5ad021c43a919032f79f87490ad6990d8d09cd
Jordan Bach <jordanbach@gmail.com>
2012-03-19 16:08:18 -0500

    changes

--- config/bash/aliases.bash
+++ config/bash/aliases.bash
@@ -1,7 +1,10 @@
 alias ls='ls -lhF'
+alias tf='tail -f'
+alias tn='tail -n'
 alias grepr='grep -d recurse -n' # recursive grep with line numbers
 alias grepo='grep -R -h -o -E' # recursive grep with only matching text printed
 alias lsenv='printenv | ack -i' # search for strings in environment variables
+alias password='head /dev/random | md5' # generate a random password
 
 # git
 alias g='git'
@@ -14,6 +17,7 @@ alias vf='open -a Finder .' # view current directory in Finder
 alias bx='bundle exec'
 alias guard='bx guard -c'
 alias sp='bx rspec'
+alias cuke='bx cucumber'
 
 # apache
 alias rs='sudo apachectl restart'

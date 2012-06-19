736964d0d9f715c693e9eb90740062f6a2d47b17
Jordan Bach <jordanbach@gmail.com>
2012-06-19 11:11:42 -0500

    Alias to view README.md as HTML in your default browser

--- config/bash/aliases.bash
+++ config/bash/aliases.bash
@@ -5,6 +5,7 @@ alias grepr='grep -d recurse -n' # recursive grep with line numbers
 alias grepo='grep -R -h -o -E' # recursive grep with only matching text printed
 alias lsenv='printenv | ack -i' # search for strings in environment variables
 alias password='head /dev/random | md5' # generate a random password
+alias readme='redcarpet README.md > readme.html; open readme.html'
 
 # git
 alias g='git'

7a947bffce883662cc39ba44512cb10e2fde7eb0
Jordan Bach <jordanbach@gmail.com>
2013-04-24 11:02:15 -0500

    Add SSH completion for hosts defined in the ssh config and fix completion for 'grb track'

---
+++ config/bash/completion.bash
@@ -0,0 +1,36 @@
+git_completion_path="/usr/local/Cellar/git/$(git --version | awk '{print $3}')/etc/bash_completion.d"
+source "$git_completion_path/git-completion.bash"
+source "$git_completion_path/git-prompt.sh"
+__git_complete g __git_main
+
+_completion_list() {
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=( $(compgen -W "$*" -- ${cur}) $(compgen -f $cur) $(compgen -f $cur/) )
+}
+
+_complete_grb() {
+  if [ -d .git ]; then
+    if [[ $COMP_LINE == 'grb track*' ]]; then
+      branches=`git branch | cut -c 3-`
+      tags=`git tag`
+      _completion_list $branches $tags
+    else
+      branches=`git branch -a | grep 'remotes/' | cut  -f '3' -d '/'`
+      _completion_list $branches
+    fi
+  fi
+}
+
+alias grbt='grb track'
+complete -F _complete_grb_remote grbt
+complete -F _complete_grb grb
+
+_complete_ssh_hosts() {
+  if [ -f ~/.ssh/config ]; then
+    ssh_hosts=`grep -E '^Host ' ~/.ssh/config | awk '{print $2}'`
+    _completion_list $ssh_hosts
+  fi
+}
+
+complete -F _complete_ssh_hosts ssh
+complete -F _complete_ssh_hosts scp
--- config/bash/git-completion.bash
+++
@@ -1,5 +0,0 @@
-git_completion_path="/usr/local/Cellar/git/$(git --version | awk '{print $3}')/etc/bash_completion.d"
-source "$git_completion_path/git-completion.bash"
-source "$git_completion_path/git-prompt.sh"
-
-__git_complete g __git_main
